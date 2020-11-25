ed1780826d08df3db5efcddfe55ee26a35e7720d
Din Spataj <proendergamer03@gmail.com>
2020-11-25 20:36:14 +0300

    Added features such as kicking and banning. I also updated the

--- commands/command.js
+++ commands/command.js
@@ -1,22 +1,41 @@
 const {prefix} = require('../config.json');
 
-module.exports = (client, aliases, callback) => {
+module.exports = (client, aliases, perms, callback) => {
 
     if(typeof aliases === 'string'){
         aliases = [aliases];
     }
     
     client.on('message', message => {
-        const {content} = message;
+        if(perms === 'admin'){
+            if(message.member.hasPermission('ADMINISTRATOR')){
+                const {content} = message;
 
-        aliases.forEach(alias => {
-            const command = `${prefix}${alias}`;
+                aliases.forEach(alias => {
+                    const command = `${prefix}${alias}`;
 
-            if(content.startsWith(`${command}`) || content === command){
-                console.log(`running the command ${command}`);
-                callback(message);
+                    if(content.startsWith(`${command}`) || content === command){
+                        console.log(`running the command ${command}`);
+                        callback(message);
+                    }
+
+                })
+            }else{
+                return;
             }
+        }else{
+            const {content} = message;
 
-        })
+            aliases.forEach(alias => {
+                const command = `${prefix}${alias}`;
+
+                if(content.startsWith(`${command}`) || content === command){
+                    console.log(`running the command ${command}`);
+                    callback(message);
+                }
+
+            })
+        }
+        
     })
 }
--- index.js
+++ index.js
@@ -2,39 +2,52 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const dotenv = require('dotenv').config();
 
-const command = require('./commands/command')
+const command = require('./commands/command');
+const logo = 'https://avatars0.githubusercontent.com/u/12706692?s=200&v=4';
 
 client.on('ready', () => {
 
+	command(client, 'embed', 'admin',(message) => {
+		const content = JSON.parse(message.content.replace('$embed ', ''));
+		const embed = new Discord.MessageEmbed()
+		.setColor('#d50c15')
+		.setTitle(message.channel.name)
+		.setAuthor(message.author.username)
+		.addFields(content)
+		.setFooter('TheNewBoston', logo);
+
+		message.channel.send(embed);
+	});
+
 	//test command
-	command(client, ['help'], (message) => {
-		if(message.member.hasPermission('ADMINISTRATOR')){
+	command(client, ['help'], 'admin',(message) => {
 			const helpEmbed = new Discord.MessageEmbed()
 				.setColor('#d50c15')
 				.setTitle('Help Menu')
-				.setAuthor('TheNewBoston', 'https://avatars0.githubusercontent.com/u/12706692?s=200&v=4', 'https://thenewboston.com/')
-				.setThumbnail('https://avatars0.githubusercontent.com/u/12706692?s=200&v=4')
+				.setAuthor('TheNewBoston', logo, 'https://thenewboston.com/')
+				.setThumbnail(logo)
 				.addFields(
 					{ name: 'Commands', value: '________' }
 				)
 				.addField('$clear', 'clears all messages in a channel', false)
-				.addField('$clear [number]', 'clears all a certain amount of messages in a channel', false)
+				.addField('$clear [number]', 'clears all a certain amount of messages in a channel (should be less than 100)', false)
 				.addField('$status [coding]', 'This sets the bots status to the specified text', false)
+				.addField('$embed [{"name":"name", "value": "value"}]', 'This creates a embed, you can seperate fields with a comma(,)', false)
+				.addField(prefix + 'ban [@member]', 'This bans the member mentioned in the command', false)
+				.addField( prefix + 'kick [@member]', 'This kicks the member mentioned in the command', false)
 				.addField('\u200B', '\u200B')
 				.setTimestamp()
-				.setFooter('TheNewBoston', 'https://avatars0.githubusercontent.com/u/12706692?s=200&v=4');
+				.setFooter('TheNewBoston', logo);
 
 			message.channel.send(helpEmbed);
-		}
 		
 	});
 
 	//clears all messages or a specified amount in a channel
-	command(client, 'clear', (message) => {
-		if(message.member.hasPermission('ADMINISTRATOR')){
+	command(client, 'clear', 'admin',(message) => {
 			const content = message.content.replace('$clear ', '');
 			if(Number.isInteger(parseInt(content))){
-				message.channel.messages.fetch({limit: content}).then(results => {
+				message.channel.messages.fetch({limit: parseInt(content)+1}).then(results => {
 					message.channel.bulkDelete(results);
 				}).catch(console.error());
 			}else{
@@ -42,12 +55,10 @@ client.on('ready', () => {
 					message.channel.bulkDelete(results);
 				}).catch(console.error());
 			}
-		}
 	});
 
 	//Sets the bots status
-	command(client, 'status', (message) => {
-		if(message.member.hasPermission('ADMINISTRATOR')){
+	command(client, 'status', 'admin',(message) => {
 			const content = message.content.replace('$status ', '');
 			client.user.setPresence(
 				{
@@ -57,8 +68,34 @@ client.on('ready', () => {
 					}
 				}
 			);
-		}
 	});
+
+	//bans members
+    command(client, 'ban', 'mods', (message) => {
+        var member = message.mentions.members.first();
+        // ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://tenor.com/view/bye-forever-leave-me-alone-gif-9357584 :point_right: ");
+        }).catch(() => {
+            // Failmessage
+            message.channel.send("Access Denied");
+        });
+    });
+
+    //bans members
+    command(client, 'kick', 'mods', (message) => {
+        // Easy way to get member object though mentions.
+        var member = message.mentions.members.first();
+        // ban
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked https://tenor.com/view/angry-birds-movie-kick-gif-5231324 :point_right: ");
+        }).catch(() => {
+            // Failmessage
+            message.channel.send("Access Denied");
+        });
+    });
 });
 
 client.login(dotenv.parsed.TOKEN);
